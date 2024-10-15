f84619b726558394cab9dcf707796797eae6a236
Dima_Kaleinik <dim040489@gmail.com>
2024-10-15 14:58:40 +0300

    added background for App

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 import './App.scss';
 import {Menu} from "./components/Menu/Menu";
 import {TDRoutes} from "./Routes";
+import {ReactComponent as BG} from "../src/images/bgDark.svg";
 
 const App: React.FC = () => {
   return (
     <div className="App">
+      <div className='App__landing'>
+        <BG/>
+      </div>
       <Menu/>
       <TDRoutes/>
     </div>
